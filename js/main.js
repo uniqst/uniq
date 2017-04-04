d645e10fc67626d71bfd40084990e425235b8faa
Galifax <galifax94@gmail.com>
2017-04-04 14:40:00 +0300

    preloader

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+document.body.onload = function(){
+    setTimeout(function(){
+        var preloader = document.getElementById('page-preloader');
+        if(!preloader.classList.contains('done')){
+            preloader.classList.add('done');
+        }
+    }, 1000);
+}
+
+
 $( document ).ready(function() {
     $("#btn").click(
 		function(){
